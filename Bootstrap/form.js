3e0992433363ce33f318a9303e0d4abe259aafb8
CLS <clindforsSejsing@gmail.com>
2022-02-24 21:41:03 +0100

    validation done

--- Bootstrap/form.js
+++ Bootstrap/form.js
@@ -1,26 +1,28 @@
-let RegCta = document.getElementById("regCta");
-
-let submitCta = document.getElementById("sCta");
+//declare variables from index.html to be able to render the login to become registrer-form. And the action-buttons.  
 
 let card = document.getElementById("card");
+let regCta = document.getElementById("registrerCta");
+let submitCta = document.getElementById("submitCta");
+let regAndSignin = document.getElementById("regAndSigninCta");
 
-RegCta.addEventListener("click", function () {
-    // window.alert("hello");
 
+//clear all html and add new by using function registrerInfo.
+regCta.addEventListener("click", function () {
     card.innerHTML = "";
     registrerInfo();
 });
 
 submitCta.addEventListener("submit", function (event) {
     event.preventDefault();
-
-    //kör kontroller här så sidan inte byts förrän alla req gått igenom.
+    window.location = "success.html";
 });
 
+//create form
 function registrerInfo() {
 
     const regForm = document.createElement("form");
     regForm.setAttribute("class", "container-sm");
+    regForm.setAttribute("id", "regForm");
     regForm.setAttribute("action", "./Bootstrap/success.html");
     card.append(regForm);
 
@@ -32,31 +34,59 @@ function registrerInfo() {
     pageId.innerText = "Registrer:";
     styleToInput.append(pageId);
 
+    //create and validate form-input
+    const regNameLabel = document.createElement("label");
     const regName = document.createElement("input");
-    regName.setAttribute("id", "name");
+    regNameLabel.setAttribute("for", "First Name");
+    regName.setAttribute("id", "First Name");
     regName.setAttribute("type", "text");
-    regName.setAttribute("placeholder", "First Name");
+    regName.setAttribute("required", "");
+    regName.setAttribute("placeholder", "Enter First Name");
     regName.setAttribute("class", "form-control col mb-3 mt-3");
+    regName.append(regNameLabel);
     styleToInput.append(regName);
 
+    const regNameLastLabel = document.createElement("label");
     const regLastName = document.createElement("input");
-    regLastName.setAttribute("id", "firstname");
+    regLastName.setAttribute("id", "Last name");
     regLastName.setAttribute("type", "text");
     regLastName.setAttribute("class", " form-control col mb-3 mt-3");
-    regLastName.setAttribute("placeholder", "Last Name");
+    regLastName.setAttribute("placeholder", "Enter Last Name");
+    regNameLastLabel.setAttribute("for", "Last Name");
+    regLastName.setAttribute("required", "");
+    regLastName.append(regNameLastLabel);
     styleToInput.append(regLastName);
 
+    const regEmailLabel = document.createElement("label");
     const regEmail = document.createElement("input");
     regEmail.setAttribute("type", "email");
     regEmail.setAttribute("class", "form-control col mb-3 mt-3");
-    regEmail.setAttribute("placeholder", "Email");
+    regEmail.setAttribute("placeholder", "Enter Email");
+    regEmail.setAttribute("required", "");
+    regEmailLabel.setAttribute("for", "Email");
+    regEmail.append(regEmailLabel);
     styleToInput.append(regEmail);
 
+    const passLabel = document.createElement("label");
     const passW = document.createElement("input");
     passW.setAttribute("type", "password");
     passW.setAttribute("class", "form-control col mb-3 mt-3");
-    passW.setAttribute("placeholder", "Password");
+    passW.setAttribute("placeholder", " Enter Password");
+    passW.setAttribute("id", "passW");
+    passLabel.setAttribute("for", "Password");
+    passW.setAttribute("required", "");
     styleToInput.append(passW);
+
+    const passConfLabel = document.createElement("label");
+    const passWConfirm = document.createElement("input");
+    passWConfirm.setAttribute("type", "password");
+    passWConfirm.setAttribute("class", "form-control col mb-3 mt-3");
+    passWConfirm.setAttribute("id", "passWConfirm");
+    passWConfirm.setAttribute("placeholder", "Repeat Password");
+    passWConfirm.setAttribute("required", "");
+    passConfLabel.setAttribute("for", "Password");
+    styleToInput.append(passWConfirm);
+
 
     const CtaLogin = document.createElement("button");
     CtaLogin.setAttribute("type", "submit");
@@ -65,10 +95,28 @@ function registrerInfo() {
     CtaLogin.innerHTML = "Save & login";
     styleToInput.append(CtaLogin);
 
-    CtaLogin.addEventListener("submit", function (event) {
-        event.preventDefault();
+    //control to se if passwords matches
+    CtaLogin.addEventListener("click", function (event) {
+        if (!passwordsMatch()) {
+            event.preventDefault();
+            return false;
+        }
+    }, false);
 
-        //kör kontroller här så sidan inte byts förrän alla req gått igenom, och då skicka tillbaka till greetingssidan. 
-    });
+    function passwordsMatch() {
+        let passValue = document.getElementById("passW").value;
+        let passConValue = document.getElementById("passWConfirm").value;
 
+        if (passValue != passConValue) {
+            let wrongPass = document.createElement("p");
+            wrongPass.setAttribute("style", "red");
+            wrongPass.innerText = "Passwords dont match! Try again."
+            styleToInput.append(wrongPass);
+            return false;
+        }
+        return true;
+    }
 }
+
+
+
